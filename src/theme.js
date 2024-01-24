889e634794370d04d81d2ebd25a1154f8e49bb4a
Tran Vinh Phat <phattran1023@gmail.com>
2024-01-24 15:57:15 +0700

    Add basic layout

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,17 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 // Create a theme instance.
 const theme = extendTheme({
+  trelloCustom : {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange
-      }
+      },
+      spacing: (factor) => `${0.25 * factor}rem` // (Bootstrap strategy)
     },
     dark: {
       palette: {
