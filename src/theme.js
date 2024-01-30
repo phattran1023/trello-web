40c06be8dbc962da0e9348bb6fe48e5695a3eb7e
Tran Vinh Phat <phattran1023@gmail.com>
2024-01-30 09:05:00 +0700

    Add styling and functionality to AppBar and ModeSelect components

--- src/theme.js
+++ src/theme.js
@@ -23,6 +23,24 @@ const theme = extendTheme({
     }
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#ccc',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894',
+            borderRadius: '8px'
+          }
+        }
+      }
+    },
     // Name of the component
     MuiButton: {
       styleOverrides: {
