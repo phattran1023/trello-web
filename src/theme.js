c8057e8931c379bbf9536399c74d2813025d4358
Tran Vinh Phat <phattran1023@gmail.com>
2024-02-11 12:44:56 +0700

    Code Board Content UI and add some CSS

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,15 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 // Create a theme instance.
 const theme = extendTheme({
   trelloCustom : {
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   // colorSchemes: {
   //   light: {
@@ -86,6 +91,13 @@ const theme = extendTheme({
         root: {
           // color: theme.palette.primary.main,
           fontSize: '0.875rem'
+        }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
         }
       }
     }
